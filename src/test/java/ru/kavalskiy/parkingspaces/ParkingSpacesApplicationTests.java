9d2f98a3d48283354edca2357e57d4514b53b50c
Skitaylov <you@example.com>
2024-04-24 22:05:14 +0300

    init: start project

---
+++ src/test/java/ru/kavalskiy/parkingspaces/ParkingSpacesApplicationTests.java
@@ -0,0 +1,13 @@
+package ru.kavalskiy.parkingspaces;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ParkingSpacesApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
